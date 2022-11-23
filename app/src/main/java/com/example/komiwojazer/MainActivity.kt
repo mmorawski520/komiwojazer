6a9558f6acf43d010a9ee87ff8401d20bdc3059e
jp2137 <sbaiyassine2004@gmail.com>
2022-11-23 09:59:45 +0100

    activity changing implementation

--- app/src/main/java/com/example/komiwojazer/MainActivity.kt
+++ app/src/main/java/com/example/komiwojazer/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.example.komiwojazer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var optionsBtn: Button;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        optionsBtn = findViewById(R.id.btnOptions)
+
+        optionsBtn.setOnClickListener {
+            val intent = Intent(this, OptionsActivity::class.java)
+            startActivity(intent)
+        }
     }
-
-
 }
--- app/src/main/java/com/example/komiwojazer/OptionsActivity.kt
+++ app/src/main/java/com/example/komiwojazer/OptionsActivity.kt
@@ -1,11 +1,22 @@
 package com.example.komiwojazer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class OptionsActivity : AppCompatActivity() {
+
+    lateinit var returnBtn:Button;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_options)
+
+        returnBtn = findViewById(R.id.btnReturn);
+
+        returnBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
